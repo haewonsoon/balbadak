4aa2c2549116992d7bddd3f9120e16becce5a9d1
haewonsoon <haewonsoon@kakao.com>
2023-03-19 17:39:47 +0900

    initial commit

---
+++ src/main/java/com/back/balbadak/BalbadakApplication.java
@@ -0,0 +1,13 @@
+package com.back.balbadak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalbadakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalbadakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/balbadak/BalbadakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.balbadak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalbadakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
