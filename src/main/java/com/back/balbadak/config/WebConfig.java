090bbcdde16ce51041da77fb2245a9fb98bc5e50
haewonsoon <haewonsoon@kakao.com>
2023-04-07 16:55:30 +0900

    Test : file List 조회 Test api

---
+++ src/main/java/com/back/balbadak/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.back.balbadak.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Value("${file.resource.path}")
+	private String resourcePath;
+
+	@Value("${file.upload.path}")
+	private String fileUploadPath;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://172.31.99.238:3000")
+				.allowedMethods("GET", "POST").maxAge(3000);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(fileUploadPath).addResourceLocations(resourcePath);
+	}
+
+}
--- src/main/java/com/back/balbadak/web/TestAPIController.java
+++ src/main/java/com/back/balbadak/web/TestAPIController.java
@@ -151,5 +151,57 @@ public class TestAPIController {
 
         return new ResponseEntity<>(base64, HttpStatus.OK);
     }
+    
+    @SuppressWarnings("unchecked")
+    @GetMapping("/fileTest3")
+    @ResponseBody
+    public ArrayList<HashMap<String,Object>> showImg2() throws IOException {
+    	ArrayList<HashMap<String,Object>> result = new ArrayList<>();
+        HashMap<String,Object> map = new HashMap<>();
+        
+    	map.put("itemFileId", "1");
+    	map.put("itemFilePath", "http://10.10.76.199:8070/upload/1.png");
+    	map.put("itemFileName", "1.png");
+        map.put("itemContent", "안녕 난 뉴욕에 사는 존이야 언제든 궁금한게 있다면, 편하게 물어봐");
+
+        result.add(0, (HashMap<String, Object>) map.clone());
+        
+    	map.put("itemFileId", "2");
+    	map.put("itemFilePath", "http://10.10.76.199:8070/upload/2.png");
+        map.put("itemFileName", "Sopia Kim");
+        map.put("itemContent", "이건 나의 손녀가 관리하는 계정입니다.");
+
+        result.add(1, (HashMap<String, Object>) map.clone());
+        
+    	map.put("itemFileId", "3");
+    	map.put("itemFilePath", "http://10.10.76.199:8070/upload/3.png");
+        map.put("itemFileName", "min son");
+        map.put("itemContent", "서울고등학교 1학년 8반 손민입니다. 반갑습니다.");
+
+        result.add(2, (HashMap<String, Object>) map.clone());
+        
+        map.put("itemFileId", "4");
+        map.put("itemFilePath", "http://10.10.76.199:8070/upload/4.png");
+    	map.put("itemFileName", "John Doe");
+        map.put("itemcontent", "안녕 난 뉴욕에 사는 존이야 언제든 궁금한게 있다면, 편하게 물어봐");
+
+        result.add(3, (HashMap<String, Object>) map.clone());
+        
+        map.put("itemFileId", "5");
+        map.put("itemFilePath", "http://10.10.76.199:8070/upload/2.png");
+        map.put("itemFileName", "Sopia Kim");
+        map.put("itemContent", "이건 나의 손녀가 관리하는 계정입니다.");
+
+        result.add(4, (HashMap<String, Object>) map.clone());
+        
+        map.put("itemFileId", "6");
+    	map.put("itemFilePath", "http://10.10.76.199:8070/upload/3.png");
+        map.put("itemFileName", "min son");
+        map.put("itemContent", "서울고등학교 1학년 8반 손민입니다. 반갑습니다.");
+
+        result.add(5, (HashMap<String, Object>) map.clone());
+    	
+    	return result;
+    }
 
 }
