d4e801df00b68dfad7fc599b7a3c85d622e08e9a
haewonsoon <haewonsoon@kakao.com>
2023-05-02 14:02:27 +0900

    Refactor

--- src/main/java/com/back/balbadak/consts/Constant.java
+++ src/main/java/com/back/balbadak/consts/Constant.java
@@ -10,6 +10,6 @@ public class Constant {
 	public static final DateTimeFormatter DEFAULT_DATETIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	public static final DateTimeFormatter SIMPLE_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyyMMdd");
 	public static final DateTimeFormatter SIMPLE_DATETIME_FORMAT = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS");
-	
 	public static final DateTimeFormatter TEXT_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+	public static final DateTimeFormatter TEXT_DATETIME_FORMAT = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분");
 }
